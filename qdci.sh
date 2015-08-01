146cf6d4427f807e9f8d967fe88356d2b700d822
Martin Cosgrave <martin@bettercode.com>
2015-08-01 18:46:34 +0200

    only cleanup after checking branch arg

--- qdci.sh
+++ qdci.sh
@@ -2,6 +2,13 @@
 
 cd "`dirname $0`"
 
+function banner {
+	echo
+	echo ===============
+	echo $*
+	echo ==========
+}
+
 if [ "$1" = "" ]
 then
 	echo Please enter the name of the branch you would like to test
@@ -13,11 +20,12 @@ then
 	exit
 fi
 
-echo Cleaning up any old VMs and config
-rm -f .git-branch.yaml
-
 # save the branch in a temporary file that the Vagrantfile can find
 echo branch: $1 > .git-branch.yaml
 
+banner Cleaning up any old VMs
 vagrant destroy -f
+sleep 9
+
+banner Bringing VMs up
 vagrant up --provider=digital_ocean ubuntu50x
