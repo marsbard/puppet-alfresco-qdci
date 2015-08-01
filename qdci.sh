6b54aabfd923a94f6ed833951a8b0e51c3e838bc
Martin Cosgrave <martin@bettercode.com>
2015-08-01 15:43:09 +0200

    tweaking

--- qdci.sh
+++ qdci.sh
@@ -1,11 +1,11 @@
 
 cd "`dirname $0`"
 
-if [ ! -d "puppet-alfresco" ]
-then
-	git clone https://github.com/marsbard/puppet-alfresco.git
-	puppet-alfresco/install/modules-for-vagrant.sh
-	mkdir modules/alfresco
-	cp -r puppet-alfresco/manifests/* modules/alfresco
-fi
+# if [ ! -d "puppet-alfresco" ]
+# then
+# 	git clone https://github.com/marsbard/puppet-alfresco.git
+# 	puppet-alfresco/install/modules-for-vagrant.sh
+# 	mkdir modules/alfresco
+# 	cp -r puppet-alfresco/manifests/* modules/alfresco
+# fi
 vagrant up --provider=digital_ocean centos42f
--- start.sh
+++ start.sh
@@ -8,6 +8,19 @@ git clone https://github.com/marsbard/puppet-alfresco.git
 
 cd puppet-alfresco
 
+install/modules-for-vagrant.sh
+
+# ensure that our module is in the right place
+if [ ! -d modules/alfresco ]
+then
+        mkdir modules/alfresco -p
+        for d in lib files manifests templates
+        do
+                ln -s ${PWD}/${d} ${PWD}/modules/alfresco/${d}
+        done
+fi
+
+
 cp /vagrant/manifests/${TYPE}.pp .
 
 puppet apply --modulepath=modules ${TYPE}.pp
