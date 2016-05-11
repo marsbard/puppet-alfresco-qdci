16ab0ed26b2c4016075b7f6bfdcbc33051d7fb68
Martin Cosgrave <martin@ciderpunk.com>
2016-05-11 11:56:38 +0200

    added test setup script

---
+++ tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git clone https://github.com/marsbard/alfresco-tests.git
+cd alfresco-tests.git
+
+cat <<EOF > config.yml
+user: admin
+passwd: admin 
+sharehost: localhost
+shareport: 8443
+repohost: localhost
+repoport: 8443
+https: true
+loginurl: /share
+browser: firefox
+photopath: images
+cmisurl: /alfresco/api/-default-/public/cmis/versions/1.1/browser
+cmisatom: /alfresco/cmisatom
+ftpport: 2021
+imap_host: localhost
+imap_port: 8143
+EOF
+
+./install.sh
+./runtests.sh
